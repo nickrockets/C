21670245325f3ef49037fb015dc55eeb124d87a6
Nikolay <54248452+nickrockets@users.noreply.github.com>
2020-09-24 21:16:33 +0200

    Add files via upload
    Order of operations in math

---
+++ math_operations.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int a = 4 + 2 * 6;
+    printf("Result 1 is %d \n", a);
+    a = (4 + 2) * 6;
+    printf("Result 2 is %d \n", a);
+    return 0;
+}
